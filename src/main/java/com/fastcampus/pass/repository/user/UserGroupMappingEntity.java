9a56452bdd6e19e03bd0f349c47780610910b2cf
hsan <heissun1113@gmail.com>
2025-02-17 22:15:01 +0900

    유저 그룹 Entity, Repository 생성

---
+++ src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingEntity.java
@@ -0,0 +1,24 @@
+package com.fastcampus.pass.repository.user;
+
+import com.fastcampus.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_group_mapping")
+@IdClass(UserGroupMappingId.class)
+public class UserGroupMappingEntity extends BaseEntity {
+    @Id
+    private String userGroupId;
+    @Id
+    private String userId;
+
+    private String userGroupName;
+    private String description;
+}
---
+++ src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingId.java
@@ -0,0 +1,17 @@
+package com.fastcampus.pass.repository.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class UserGroupMappingId implements Serializable {
+  private String userGroupId;
+  private String userId;
+}

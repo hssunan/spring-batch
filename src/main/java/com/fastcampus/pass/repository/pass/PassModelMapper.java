bec94951d61d8799f85e3d59264e6fc670eaebff
hsan <heissun1113@gmail.com>
2025-02-17 22:16:35 +0900

    Entity 필드 매핑 유틸 ModelMapper 인터페이스 생성

---
+++ src/main/java/com/fastcampus/pass/repository/pass/PassModelMapper.java
@@ -0,0 +1,26 @@
+package com.fastcampus.pass.repository.pass;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+// 일치하지 않는 필드 무시
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PassModelMapper {
+
+    PassModelMapper INSTANCE = Mappers.getMapper(PassModelMapper.class);
+
+    // 필드명이 같지 않거나 custom하게 매핑해주기 위해서는 @Mapping 추가
+    @Mapping(target = "status", qualifiedByName = "defaultStatus")
+    @Mapping(target = "remainingCount", source = "bulkPassEntity.count")
+    PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId);
+
+    // BulkPassStatus와 관계 없이 PassStatus값을 설정
+    @Named("defaultStatus")
+    default PassStatus status(BulkPassStatus status) {
+        return PassStatus.READY;
+    }
+
+}
